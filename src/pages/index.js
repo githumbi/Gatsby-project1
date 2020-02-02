12aeefa1a4e0fcc782cf0c19b859ef65a2df5aaf
githumbi <githumbi74@gmail.com>
2020-02-02 15:58:28 +0300

    styling using css-in js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import Header from "../components/header"
 import { Link } from "gatsby"
 
-export default () => <div style={{color: 'purple', fontSize: '72px'}}>
-    <Header headerText="Hello  Gatsby" />
+export default () => <div style={{ fontSize: '72px'}}>
+    <Header headerText="Hello  jose" />
   <Link to="/contact">Contact</Link>
     <p>paragraph</p>
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Dog_coat_variation.png/220px-Dog_coat_variation.png" alt="dog"/>
